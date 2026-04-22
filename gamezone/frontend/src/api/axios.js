41b5adac46f9e24eb41ee36a9ad6063df0bbd96d
Hasti Desai <desaihasti06@gmail.com>
2026-04-22 13:48:37 +0530

    update url

--- gamezone/frontend/src/api/axios.js
+++ gamezone/frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL:"https://game-zone-lmi6.onrender.com/api",
 });
 
 // Attach JWT token to every request
